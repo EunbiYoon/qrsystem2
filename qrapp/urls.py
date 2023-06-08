d71ebf88847e3863361e657ed5d68e454449480a
Eunbi Yoon <yinyinbei0717@gmail.com>
2023-06-08 16:00:37 -0500

    werer

--- qrapp/urls.py
+++ qrapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import homeView,searchView,scanView,genView
 
 urlpatterns = [
-    path('',homeView,name='home_url'),
-    path('search/',searchView, name='search_url'),
+    path('',homeView.as_view(),name='home_url'),
+    path('search/',searchView.as_view(), name='search_url'),
     path('scan/',scanView,name='scan_url'),
     path('add/',genView,name='add_url')
 ]
--- qrapp/views.py
+++ qrapp/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.views import View
 from .models import QRCodeData
+from django.views.generic import TemplateView, ListView
 # Create your views here.
 
-def homeView(request):
-    return render(request,'index.html')
+class homeView(TemplateView):
+    template_name='index.html'
 
-def searchView(request):
-    return render(request,'search.html')
+class searchView(ListView):
+    template_name='search.html'
+    context_object_name='item_list'
+    def get_queryset(self):
+        return QRCodeData.objects.all()
 
 def scanView(request):
     return render(request,'scan.html')
