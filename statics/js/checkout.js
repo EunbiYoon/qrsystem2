43b9c0afba76cb716ffbcd1d240b9d2696ef28ad
Eunbi Yoon <yinyinbei0717@gmail.com>
2023-06-23 09:23:10 -0500

    wrer

---
+++ statics/js/checkout.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const scanner= new Html5QrcodeScanner(
+        'reader',{
+        qrbox:{
+            width:250,
+            height:250,
+        },
+        fps:20,
+    });
+    scanner.render(success);
+});
+
+function success(result) {
+    /*change value*/
+    var input=document.getElementById("result");
+    input.value=result;
+
+    /*change reader to success message*/
+    document.getElementById('reader').innerHTML = `
+    <h2>Success!</h2>
+    <p><a href="${result}">${result}</a></p>
+    `;
+
+    scanner.clear();
+}
+function error(err){
+    console.error(err);
+}
+
