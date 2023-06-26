afda0efd239c51ec80e47f6e3c96f8e76c684bfb
Eunbi Yoon <yinyinbei0717@gmail.com>
2023-06-26 13:43:51 -0500

    werew

--- statics/js/qrscan2.js
+++ statics/js/qrscan2.js
@@ -11,20 +11,31 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 
 function success(result) {
-    /*change reader to success message*/
-    document.getElementById('reader').innerHTML = ``;
+    /*change reader to success message*/ 
+    var reader=document.getElementById('reader')
+    reader.innerHTML = ``;
     document.getElementById('choose').innerHTML = ``;
     
+    /*change reader to success message*/
+    document.getElementById('info').innerHTML = ``;
+
     /*change value*/
     var input=document.getElementById("result");
     input.value=result;
 
+    /* remove the border*/
+    reader.style.border='none';
+
+    /* reshape and make border*/
+    reader.style.border='none';
+    input.style.borderRadius='15px';
+
+    /* cleanup the scanner*/
+    scanner.clear();
     // document.getElementById('reader').innerHTML = `
     // <h2>Success!</h2>
     // <p><a href="${result}">${result}</a></p>
     // `;
-
-    scanner.clear();
 }
 function error(err){
     console.error(err);
